9b8c1c34c9fbfdb8a854dcaae892e41f98ecf2d6
Jky0 <62363574+Jky0@users.noreply.github.com>
2020-10-09 12:10:22 -0500

    uso de selenium

--- Selenium/src/main/java/CoolTesters/Selenium/App.java
+++ Selenium/src/main/java/CoolTesters/Selenium/App.java
@@ -2,29 +2,103 @@ package CoolTesters.Selenium;
 
 import java.io.File;
 import java.nio.file.Paths;
-import java.util.List;
+
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
-/**
- * Hello world!
- *
- */
+
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws InterruptedException
         
     {
+    	   	
+    	System.out.println("Curso Septiembre 05.09.2020");
     	
-   	
-//    	System.out.println("Curso Septiembre 05.09.2020");
-//    	String exePath = Paths.get("").toAbsolutePath().toString() + File.separator + "driver" + File.separator; //tomando el path del driver en el sistema
-//    	System.setProperty("webdriver.chrome.driver", exePath + "chromedriver.exe");// Establecer la propiedad del .exe
-//    	WebDriver driver = new ChromeDriver(); // Inicializando el driver
-//    	driver.get("http://www.cooltesters.com/tienda-de-cursos/selenium-con-java");
-//    	
+    	String exePath = Paths.get("").toAbsolutePath().toString() + File.separator + "driver" + File.separator; //tomando el path del driver en el sistema
+    	System.setProperty("webdriver.chrome.driver", exePath + "chromedriver.exe");// Establecer la propiedad del .exe
+    	WebDriver driver = new ChromeDriver(); // Inicializando el driver
+    	driver.get("https://opensource-demo.orangehrmlive.com/"); //Iniciar chrome en la URL especificada
+    	//driver.manage().window().maximize();//Maximizar Ventana
+    	
+    	
+    	//Obteniendo objetos / elementos de la pagina de login
+    	
+    	WebElement txtUserName = driver.findElement(By.id("txtUsername"));
+    	WebElement txtpassword = driver.findElement(By.id("txtPassword"));
+    	WebElement btnLogin = driver.findElement(By.id("btnLogin"));
+    	
+    	
+    	//Haciendo login
+    	
+    	txtUserName.sendKeys("Admin");
+    	Thread.sleep(3000);
+    	txtpassword.sendKeys("admin123");
+    	Thread.sleep(3000);
+    	btnLogin.click();
+    	Thread.sleep(5000);
+    	
+    	//Validacion de Home Page
+    	
+    	String urlDashboard = driver.getCurrentUrl();
+    	System.out.println("URL: " + urlDashboard);
+    	urlDashboard.contains("/dashboard"); // Verificando que la URL contenga /dashboard
+    	
+    	// Metodos de navegación
+    	
+//    	driver.navigate().back();
+//    	Thread.sleep(3000);
+//    	driver.navigate().forward();
+//    	Thread.sleep(3000);
+//    	driver.navigate().refresh();
+//    	Thread.sleep(3000);
+    	
+    	// Como imprimir el titulo de la pagina
+    	
+    	String title = driver.getTitle();
+    	System.out.println("El titulo de la pagina es: " + title);
+    	
+    	//Click assignLeaveBtn
+    	
+    	WebElement btnAssignLeave = driver.findElement(By.className("quickLinkText"));
+    	btnAssignLeave.click();
+    	Thread.sleep(3000);
+    	
+    	
+    	// Obtener Texto de un Web Element
+    	
+    	String welcomeMsj = driver.findElement(By.id("welcome")).getText();
+    	boolean verifywelcomeMsj = welcomeMsj.contains("Linda");
+    	if (verifywelcomeMsj) {
+    		System.out.println("El mensaje contiene el nombre de Linda");
+    	}else {
+    		
+    		System.out.println("Error el mensaje NO contiene el nombre esperado");
+    	}
+    	
+    	// Dropdowns
+
+    	Select oSelect = new Select(driver.findElement(By.id("assignleave_txtLeaveType")));
+    	oSelect.selectByVisibleText("Maternity US");
+    	Thread.sleep(3000);
+	
+    	
+    	
+        // Cerrar Navegador Web
+    	
+    	driver.close(); // Cierra la ventana actual
+    	driver.quit();  // Cierra todas las ventanas creadas por el WebDriver
+    	
+    	
+    }
+    
+}
+    	
+    	
     	
 //    	byte x;
 //    	
@@ -78,35 +152,35 @@ public class App
     	/*
     	 * Operadores relacionales y condicionales
     	 */
-    	
-    	int x = 7;
-    	int z = 8;
-    	
-    	if (x==z) {
-    		System.out.println("x es igual a z");
-    	}
-    	else {
-    		System.out.println("x es diferente de z");
-    	}
-    	
+//    	
+//    	int x = 7;
+//    	int z = 8;
+//    	
+//    	if (x==z) {
+//    		System.out.println("x es igual a z");
+//    	}
+//    	else {
+//    		System.out.println("x es diferente de z");
+//    	}
+//    	
         	
-    	//FindElements
-    	List<WebElement> firstLevelMenu = driver.findElements(By.xpath("//*[@class='firstLevelMenu']/b"));
-    	System.out.println("Numero de elementos: " + firstLevelMenu.size());
-    	String primerMenu = firstLevelMenu.get(0).getText();
-    	System.out.println(primerMenu);
-    	String segundoMenu = firstLevelMenu.get(1).getText();
-    	System.out.println(segundoMenu);
+//    	//FindElements
+//    	List<WebElement> firstLevelMenu = driver.findElements(By.xpath("//*[@class='firstLevelMenu']/b"));
+//    	System.out.println("Numero de elementos: " + firstLevelMenu.size());
+//    	String primerMenu = firstLevelMenu.get(0).getText();
+//    	System.out.println(primerMenu);
+//    	String segundoMenu = firstLevelMenu.get(1).getText();
+//    	System.out.println(segundoMenu);
+//    	
+//    	for(int i = 0; i < firstLevelMenu.size(); i++ ) {
+//    		System.out.println("Los menus son: " + firstLevelMenu.get(i).getText());
+//    	}
+//    	
+//    	
     	
-    	for(int i = 0; i < firstLevelMenu.size(); i++ ) {
-    		System.out.println("Los menus son: " + firstLevelMenu.get(i).getText());
-    	}
-    	
-    	
-    	
-    	
-    }
-}
+//    	
+//    }
+//}
 
 
 
